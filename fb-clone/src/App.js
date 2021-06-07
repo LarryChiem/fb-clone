edfca5065701415ec931ca8c647469c99da3f8ff
Larry Chiem <clarry@pdx.edu>
2021-06-06 23:23:34 -0700

    Working on header

---
+++ fb-clone/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <h1>Let's build the facebook clone!</h1>
+
+      <Header /> 
+
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
+
+    </div>
+  );
+}
+
+export default App;
