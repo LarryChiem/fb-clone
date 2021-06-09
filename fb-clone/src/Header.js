3bcc270fd859483966f593044793e794376b7f82
Larry Chiem <clarry@pdx.edu>
2021-06-09 00:41:45 -0700

    header still

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,7 +1,17 @@
- import React from 'react'
- import "./Header.css"
+ import React from 'react';
+ import "./Header.css";
  import SearchIcon from '@material-ui/icons/Search'; 
- 
+ import HomeIcon from '@material-ui/icons/Home'; 
+ import FlagIcon from '@material-ui/icons/Flag';
+ import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+ import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+ import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+ import { Avatar, IconButton } from"@material-ui/core";
+ import AddIcon from "@material-ui/icons/Add";
+ import ForumIcon from '@material-ui/icons/Forum';
+ import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
  function Header() {
      return (
          <div className="header">
@@ -14,8 +24,42 @@
             </div>
 
             <div className="header__middle"></div>
+                <div className="header__option">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
 
-            <div className="header__right"></div>
+
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Larry Chiem</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+            </div>
          </div>
      )
  }
