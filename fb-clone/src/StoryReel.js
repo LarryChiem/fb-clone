40c08714771842b4a40def19eb0c1fb6c1ef3cc9
Larry Chiem <clarry@pdx.edu>
2021-08-16 14:54:27 -0700

    1:39:42 Still working on storyboard
    Signed-off-by: Larry Chiem <clarry@pdx.edu>

---
+++ fb-clone/src/StoryReel.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Story from "./Story";
+import './StoryReel.css';
+
+function StoryReel() {
+    return (
+        <div className='storyReel'>
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
+                title="Larry Chiem"
+            />
+            <Story
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
+                profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
+                title="Larry Chiem"
+            />
+            <Story
+                image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                profileSrc="https://image.cnbcfm.com/api/v1/image/106569797-1591649109683gettyimages-1032942656.jpeg?v=1597932630"
+                title="Frankie"
+            />
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
+                title="Larry Chiem"
+            />
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
+                title="Larry Chiem"
+            />
+           {/* Story */} 
+           {/* Story */} 
+           {/* Story */} 
+           {/* Story */} 
+           {/* Story */} 
+
+        </div>
+    )
+}
+
+export default StoryReel
