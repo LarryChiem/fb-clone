15b1f384fc90f9eb83f8606f1328939d488178fc
Larry Chiem <clarry@pdx.edu>
2021-08-24 09:18:59 -0700

    1:44:54 Story H4 in Story.css formatting
    Signed-off-by: Larry Chiem <clarry@pdx.edu>

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -6,9 +6,9 @@ function StoryReel() {
     return (
         <div className='storyReel'>
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
                 profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
-                title="Larry Chiem"
+                title="Carry Chiem"
             />
             <Story
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
@@ -18,17 +18,17 @@ function StoryReel() {
             <Story
                 image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
                 profileSrc="https://image.cnbcfm.com/api/v1/image/106569797-1591649109683gettyimages-1032942656.jpeg?v=1597932630"
-                title="Frankie"
+                title="Harry Chiem"
             />
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
                 profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
-                title="Larry Chiem"
+                title="Mary Chiem"
             />
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
                 profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
-                title="Larry Chiem"
+                title="Fairy Chiem"
             />
            {/* Story */} 
            {/* Story */} 
