57ab6cca3022b48c7539a429e1bed272662aa7ef
Larry Chiem <clarry@pdx.edu>
2021-08-27 23:02:17 -0700

    1:47:33 StoryReel.js /css
    Signed-off-by: Larry Chiem <clarry@pdx.edu>

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -6,7 +6,7 @@ function StoryReel() {
     return (
         <div className='storyReel'>
             <Story
-                image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                image="https://scontent.fhio2-1.fna.fbcdn.net/v/t1.6435-9/61620601_2182355108468755_7812878233950486528_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=84a396&_nc_ohc=7V7whCiiDsAAX_zt1SI&_nc_ht=scontent.fhio2-1.fna&oh=42d9cf43b0df73a1134a1ee2bef9b87b&oe=614E8FEA"
                 profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
                 title="Carry Chiem"
             />
