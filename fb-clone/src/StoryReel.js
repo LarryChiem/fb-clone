196417d5b1875d8456155f9e47fb87acccc9a455
Larry Chiem <clarry@pdx.edu>
2021-09-02 00:14:02 -0700

    Customized the images and story to reflect my history
    Signed-off-by: Larry Chiem <clarry@pdx.edu>

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -8,27 +8,27 @@ function StoryReel() {
             <Story
                 image="https://scontent.fhio2-1.fna.fbcdn.net/v/t1.6435-9/61620601_2182355108468755_7812878233950486528_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=84a396&_nc_ohc=7V7whCiiDsAAX_zt1SI&_nc_ht=scontent.fhio2-1.fna&oh=42d9cf43b0df73a1134a1ee2bef9b87b&oe=614E8FEA"
                 profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
-                title="Carry Chiem"
-            />
-            <Story
-                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
-                profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
                 title="Larry Chiem"
             />
             <Story
-                image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-                profileSrc="https://image.cnbcfm.com/api/v1/image/106569797-1591649109683gettyimages-1032942656.jpeg?v=1597932630"
-                title="Harry Chiem"
+                image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.BM95X4vtpWOx-4lCjZRkuAHaEu%26pid%3DApi&f=1"
+                profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.logojoy.com%2Fwp-content%2Fuploads%2F2017%2F07%2FBoeing_logo_2.png&f=1&nofb=1"
+                title="Boeing"
             />
             <Story
-                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
-                profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
-                title="Mary Chiem"
+                image="https://trimet.org/global/includes/img/promo-hiring.jpg"
+                profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb3%2FTriMet_icon.svg%2F1200px-TriMet_icon.svg.png&f=1&nofb=1"
+                title="Trimet"
             />
             <Story
-                image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-                profileSrc="https://avatars.githubusercontent.com/u/15273129?v=4"
-                title="Fairy Chiem"
+                image="https://scontent.fhio2-2.fna.fbcdn.net/v/t1.6435-9/176729381_4258029467650388_4358150702190357704_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=a26aad&_nc_ohc=eEMBq0t2ixcAX-ZcpUq&_nc_ht=scontent.fhio2-2.fna&oh=83cdc697698fc03966657b3c4dcc0a7d&oe=6156B96D"
+                profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frandomc.net%2Fimage%2FHaikyuu%2FHaikyuu%2520Second%2520Season%2520-%252003%2520-%2520Large%252026.jpg&f=1&nofb=1"
+                title="Volleyball Chiem"
+            />
+            <Story
+                image="https://scontent.fhio2-2.fna.fbcdn.net/v/t1.6435-9/38030595_2182352071793921_8370502810176847872_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=HGHNas54IBUAX8y_fBN&_nc_ht=scontent.fhio2-2.fna&oh=4a4b4cd3ac75cf62192b96734ed8ded7&oe=6154AE9C"
+                profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.PIZoLdX5zRbL-m7lbra9egHaFR%26pid%3DApi&f=1"
+                title="Intel"
             />
            {/* Story */} 
            {/* Story */} 
